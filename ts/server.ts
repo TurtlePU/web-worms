d9117357af7bd8e4212066023786e81c67874a05
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-06 18:23:17 +0300

    + code

--- ts/server.ts
+++ ts/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_, response) => {
+    response.sendFile('./client/page/index.html');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
