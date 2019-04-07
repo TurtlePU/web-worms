02af50c85625922b0991f9800952111f91d96909
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-07 04:53:01 +0300

    debug + base functionality

--- ts/server.ts
+++ ts/server.ts
@@ -1,12 +1,14 @@
+import path from 'path';
+
 import express from 'express';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_, response) => {
-    response.sendFile('./client/page/index.html');
+    response.sendFile(path.join(__dirname, '../client/page/index.html'));
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
