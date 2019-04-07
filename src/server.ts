fce20521461340740c1e1e852fc338c2b6e161f1
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-08 00:48:16 +0300

    ts -> src

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_, response) => {
-    response.sendFile(path.join(__dirname, '../client/page/index.html'));
+    response.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.use(express.static('client/page'));
+app.use(express.static('client'));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
