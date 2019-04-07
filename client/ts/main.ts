e87d2bcea19f33d612abd2b3095375d90a1714f8
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-07 04:53:20 +0300

    optimized min code

--- client/ts/main.ts
+++ client/ts/main.ts
@@ -1,13 +1,8 @@
 ///<reference path="lib/pixi.js.d.ts"/>
 
-import Loader from './lib/loader.js';
-
 document.addEventListener('DOMContentLoaded', main);
 
 async function main() {
     const app = new PIXI.Application();
     document.body.appendChild(app.view);
-
-    const loader = new Loader();
-    const result = await loader.loadAsync();
 }
