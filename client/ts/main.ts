83c70d832384b352475ae4ebaf212adb5fe62d00
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-07 16:14:44 +0300

    routing structure

--- client/ts/main.ts
+++ client/ts/main.ts
@@ -1,3 +1,22 @@
-window.onload = async function() {
+import { Router } from './lib/router.js';
+import { $, setView } from './lib/html.util.js';
+
+export function RootHandler() {
+    let rootHtml = (<HTMLScriptElement> $('root')).innerHTML;
+    setView(rootHtml);
+    let button = <HTMLButtonElement> $('root-to-index');
+    button.addEventListener('click', () => Router.navigate('index'));
     console.log('KOK --- main');
-};
+}
+
+import { IndexHandler } from './views/index.js';
+
+window.addEventListener('load', async () => {
+    Router.add({
+        matcher: '',
+        handler: RootHandler
+    }).add({
+        matcher: 'index',
+        handler: IndexHandler
+    }).navigate();
+});
--- client/ts/views/index.ts
+++ client/ts/views/index.ts
@@ -1,3 +1,10 @@
-export default function() {
+import { Router } from '../lib/router.js';
+import { $, setView } from '../lib/html.util.js';
+
+export function IndexHandler() {
+    let indexHtml = (<HTMLScriptElement> $('index')).innerHTML;
+    setView(indexHtml);
+    let button = <HTMLButtonElement> $('index-to-root');
+    button.addEventListener('click', () => Router.navigate());
     console.log('KOK --- index');
 }
