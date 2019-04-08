c0e884d54bae713f21fa0725f8ed2f4de89c80ae
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-08 14:49:47 +0300

    join view

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,10 @@
 import { Router } from './lib/turtle/router.js';
 
-import { RootView } from './views/root.js';
-import { IndexView } from './views/index.js';
+import { JoinView } from './views/join.js';
 
 window.addEventListener('load', async () => {
-    console.log(Cookies.get('view'));
+    console.log(`saved page: '${Cookies.get('view')}'`);
     Router
-        .add(new RootView())
-        .add(new IndexView())
+        .add(new JoinView())
         .navigate(Cookies.get('view'));
 });
