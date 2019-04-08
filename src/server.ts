45f6361ab6f3e130f4b6c64a462b5a9134454b8f
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-08 19:10:37 +0300

    server: codestyle + room handling

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,25 @@
+import express from 'express';
 import path from 'path';
 
-import express from 'express';
+import { RoomHandler } from './lib/rooms';
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
-app.get('/', (_, response) => {
-    response.sendFile(path.join(__dirname, '../client/index.html'));
+app.get('/method=getRoom', (_, res) => {
+    res.send({ id: RoomHandler.getRoom() });
+});
+
+app.get('/method=checkRoom/:id', (req, res) => {
+    res.send({ exists: RoomHandler.checkRoom(req.params.id) });
 });
 
 app.use(express.static('client'));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
