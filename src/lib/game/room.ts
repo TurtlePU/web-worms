a1c09d6c67faa44ddc05cae99190faf664252662
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-09 20:24:57 +0300

    + Room logic

--- src/lib/game/room.ts
+++ src/lib/game/room.ts
@@ -1,7 +1,12 @@
 import ShortID from 'shortid';
+import { Socket } from 'socket.io';
+import { EventEmitter } from 'events';
+
+// Don't forget to emit the events!
+export type RoomEvent = 'state:lobby' | 'state:full' | 'state:game' | 'state:end';
 
 /** Game room class. */
-export default class Room {
+export default class Room extends EventEmitter {
     /**
      * Unique short identifier.
      */
@@ -12,13 +17,42 @@ export default class Room {
      * Creates new Room with random ID.
      */
     constructor() {
+        super();
         this.ID = ShortID.generate();
     }
 
+    emit(event: RoomEvent): boolean {
+        return super.emit(event);
+    }
+
+    on(event: RoomEvent, listener: (...args: any[]) => void): this {
+        return super.on(event, listener);
+    }
+
     /**
-     * @returns true if this room is full, false otherwise
+     * Adds necessary game event listeners to the socket.
+     * 
+     * @param socket 
      */
-    full() {
-        return false;
+    capture(socket: Socket): void {
+        // TODO
+    }
+    
+    /**
+     * @param socketID 
+     * @returns true if given socket was connected to this game, false otherwise
+     */
+    has(socketID: string): boolean {
+        // TODO
+        return true;
+    }
+
+    /**
+     * Releases given socket in some way.
+     * 
+     * @param socket 
+     */
+    release(socket: Socket): void {
+        // TODO
     }
 };
--- src/lib/intro/socket.ts
+++ src/lib/intro/socket.ts
@@ -1,5 +1,8 @@
 import socket from 'socket.io';
 
+import { RoomHandler } from './rooms';
+import Room from '../game/room';
+
 /**
  * Wrapper on Socket server for game needs.
  * 
@@ -11,9 +14,18 @@ export default function IO(server: any) {
 
     io.on('connection', socket => {
         console.log('new socket connected');
+        let room: Room;
+
+        socket.on('res:getRoom', roomID => {
+            room = RoomHandler.getRoom(roomID);
+            room.capture(socket);
+        });
+        socket.emit('req:getRoom');
+
         socket.on('disconnect', () => {
+            room.release(socket);
             console.log('socket disconnected');
-        })
+        });
     });
 
     return io;
