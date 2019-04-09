15c66b017dedf2c86341c4f23e994b927e36fa06
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-09 17:47:34 +0300

    + Room class on server

---
+++ src/lib/game/room.ts
@@ -0,0 +1,22 @@
+/** Game room class. */
+export default class Room {
+    /**
+     * Unique identifier in human-readable format.
+     */
+    id: string;
+
+    /**
+     * @constructor
+     * Creates new Room with random ID.
+     */
+    constructor() {
+        //
+    }
+
+    /**
+     * @returns true if this room is full, false otherwise
+     */
+    full() {
+        return false;
+    }
+};
