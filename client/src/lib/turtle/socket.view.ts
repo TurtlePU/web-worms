968d75500762efaabf452e11b32debe79357af0d
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-10 13:43:38 +0300

    + Turtle.SocketView

---
+++ client/src/lib/turtle/socket.view.ts
@@ -0,0 +1,38 @@
+///<reference path='../socket.io.d.ts'/>
+
+import { View } from './main.js';
+
+export class SocketView extends View {
+    /** Socket from which to listen / whom to emit */
+    protected socket: SocketIOClient.Socket;
+
+    /**
+     * Simple wrapper for socket requests.
+     * 
+     * @param request - unique base for event names 
+     * @param args - request parameters
+     */
+    protected async socketRequest(request: string, ...args: any) {
+        return new Promise((resolve, reject) => {
+            this.socket.once(`${request}:res`, resolve);
+            this.socket.emit(`${request}:req`, ...args);
+            setTimeout(() => {
+                console.log(`${this.ID}.socketRequest: Connection timed out`);
+                reject();
+            }, 10 * 1000);
+        });
+    }
+
+    /**
+     * Loads the view.
+     * @override
+     * 
+     * @param path - path from which View was loaded 
+     * @param socket - socket for listening
+     * @param args - any load properties defined in subclasses
+     */
+    load(path: string, socket: SocketIOClient.Socket, ...args: any) {
+        super.load(path);
+        this.socket = socket;
+    }
+}
