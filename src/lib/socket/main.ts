853d3279a3fa39af67bd963335eb035a56b3897c
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-12 11:38:07 +0300

    server/socket: odd class instead of function

--- src/lib/socket/main.ts
+++ src/lib/socket/main.ts
@@ -3,23 +3,21 @@ import { LobbyHandler } from './lobby/handler';
 import { RoomHandler } from './room/handler';
 import Requester from './requester';
 
-class SocketHandler {
-    constructor(socket: Socket) {
-        console.log('new socket connected');
+function handleSocket(socket: Socket) {
+    console.log('new socket connected');
 
-        new Requester('join')
-            .on('getLobby', LobbyHandler.getLobbyID)
-            .on('checkLobby', LobbyHandler.checkLobby)
-            .on('checkRoom', (roomID: string, socketID: string) => {
-                return RoomHandler.getRoom(roomID) &&
-                       RoomHandler.getRoom(roomID).has(socketID);
-            })
-            .apply(socket);
+    new Requester('join')
+        .on('getLobby', LobbyHandler.getLobbyID)
+        .on('checkLobby', LobbyHandler.checkLobby)
+        .on('checkRoom', (roomID: string, socketID: string) => {
+            return RoomHandler.getRoom(roomID) &&
+                    RoomHandler.getRoom(roomID).has(socketID);
+        })
+        .apply(socket);
 
-        socket.on('disconnect', () => {
-            console.log('socket disconnected');
-        });
-    }
+    socket.on('disconnect', () => {
+        console.log('socket disconnected');
+    });
 }
 
 /**
@@ -30,6 +28,6 @@ class SocketHandler {
  */
 export default function IO(server: any) {
     const io = socket(server);
-    io.on('connection', socket => new SocketHandler(socket));
+    io.on('connection', handleSocket);
     return io;
 }
