7a8033109d1f398b6ffb0d2fa917740125496152
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-15 12:56:59 +0300

    client/socket/wrapper fix for singleton use

--- client/src/lib/socket/wrapper.ts
+++ client/src/lib/socket/wrapper.ts
@@ -1,31 +1,33 @@
 ///<reference path='socket.io.d.ts'/>
 ///<reference path='socket.wrap.d.ts'/>
 
-export default function() {
-    const socket = io();
-    var channel = '';
+var socket: SocketIOClient.Socket;
+var channel = '';
 
-    socket.channel = (name: string) => {
-        channel = name;
-        return socket;
-    };
-
-    socket.request = async (request: string, ...args: any[]) => {
-        let fullname = `${channel}:${request}`;
-        console.log(`socket.request <= ${fullname}`);
-        return new Promise((resolve, reject) => {
-            let timeout: number;
-            socket.once(`${fullname}:res`, (...args: any) => {
-                window.clearTimeout(timeout);
-                console.log(`socket.request =>`, ...args);
-                resolve(...args);
-            });
-            socket.emit(`${fullname}:req`, ...args);
-            timeout = window.setTimeout(() => {
-                reject(`${fullname}: Connection timed out`);
-            }, 10 * 1000);
-        });
-    };
-
+socket.channel = (name: string) => {
+    channel = name;
     return socket;
+};
+
+socket.request = async (request: string, ...args: any[]) => {
+    let fullname = `${channel}:${request}`;
+    console.log(`socket.request <= ${fullname}`);
+    return new Promise((resolve, reject) => {
+        let timeout: number;
+        socket.once(`${fullname}:res`, (...args: any) => {
+            window.clearTimeout(timeout);
+            console.log(`socket.request =>`, ...args);
+            resolve(...args);
+        });
+        socket.emit(`${fullname}:req`, ...args);
+        timeout = window.setTimeout(() => {
+            reject(`${fullname}: Connection timed out`);
+        }, 10 * 1000);
+    });
+};
+
+export function init() {
+    socket = io();
 }
+
+export default socket;
