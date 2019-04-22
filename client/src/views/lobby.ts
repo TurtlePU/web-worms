1a9a7a71da37da07350171f690693d1510152d3d
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-22 20:02:49 +0300

    client/lobby: functionality, list of players

--- client/src/views/lobby.ts
+++ client/src/views/lobby.ts
@@ -6,28 +6,45 @@ import Router from '../lib/turtle/router.js';
 const html = /* html */`
     <h1>Lobby</h1>
     <button id='back'>Back</button>
+    <ol id='mems'></ol>
 `;
 
+function listNode(text: string) {
+    return /*html*/`<li>${text}</li>`
+};
+
+function fail(message: string) {
+    alert(message);
+    Router.navigate('join');
+}
+
 export default class LobbyView extends View {
-    // TODO: LobbyView
     private lobbyID: string;
+    private members: HTMLUListElement;
 
     constructor() {
         super('lobby', html);
+        // TODO: lobby change events
     }
 
     async load(path: string, lobbyID: string) {
-        let exists = await socket.channel('lobby')
-            .request('check', lobbyID);
-        if (!exists) {
-            alert(`Such lobby doesn't exist: ${lobbyID}`);
-            Router.navigate('join');
-        }
-
         super.load(path);
         this.lobbyID = lobbyID;
 
         let back = <HTMLButtonElement> $('back');
         back.onclick = () => Router.navigate('join');
+
+        socket.channel('lobby');
+        if (!await socket.request('check', lobbyID)) {
+            return fail(`Such lobby doesn't exist: ${lobbyID}`);
+        }
+        if (!await socket.request('join', lobbyID)) {
+            return fail(`Lobby is full: ${lobbyID}`);
+        }
+
+        this.members = <HTMLUListElement> $('mems');
+        for (let mem of await socket.request('members', lobbyID)) {
+            this.members.insertAdjacentHTML('beforeend', listNode(mem));
+        }
     }
 }
