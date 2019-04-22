2d1defae55a5216f494aff0361af2ad1a4c64e27
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-23 00:05:23 +0300

    client/main: - unused log

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,8 +6,6 @@ import LobbyView from './views/lobby.js';
 import RoomView  from './views/room.js';
 
 window.onload = async () => {
-    console.log(`Saved page: '${Cookies.get('view')}'`);
-
     initSocket();
 
     const joinView  = new JoinView();
