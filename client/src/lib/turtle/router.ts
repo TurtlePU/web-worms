40c3fc3cf5cce575f421681da63f4d3cf08e32ee
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-04-23 11:01:19 +0300

    Router: now classic Singleton

--- client/src/lib/turtle/router.ts
+++ client/src/lib/turtle/router.ts
@@ -2,21 +2,57 @@
 // Made by Turtle, P.U. in 2019
 // Based on http://krasimirtsonev.com/blog/article/A-modern-JavaScript-router-in-100-lines-history-api-pushState-hash-url
 
-// TODO: classic Singleton. Tired of one object split in 3 parts.
-
 type RouterEntry = {
     matcher: RegExp,
     handler: Function
 }
 
-/** Interface of a Router. */
-interface Router {
+function clearSlashes(path: string) {
+    return path.replace(/\/$/, '').replace(/^\//, '');
+}
+
+function toRegExp(pattern: string | RegExp) {
+    return new RegExp(
+        typeof pattern === 'string'
+            ? `^${pattern}$`
+            : pattern
+    );
+}
+
+function getFragment() {
+    let match = window.location.href.match(/#(.*)$/);
+    let fragment = match ? match[1] : '';
+    return clearSlashes(fragment);
+}
+
+class Router {
+    private routes = [] as RouterEntry[];
+    private _root = '';
+    private listening = false;
+    private current = '';
+
+    constructor() {
+        this.listener = this.listener.bind(this);
+    }
+
+    private listener() {
+        let fragment = getFragment();
+        if (this.current != fragment) {
+            console.log(`HashChange <= '${fragment}'`);
+            this.current = fragment;
+            this.check();
+        }
+    }
+
     /**
      * Sets redirection for failed checks (aka start page).
      * @param root - redirection path
      * @returns Router
      */
-    root(root: string): this,
+    root(root: string) {
+        this._root = root;
+        return this;
+    }
 
     /**
      * Adds new route to be controlled by Router.
@@ -24,32 +60,69 @@ interface Router {
      * @param handler - function called when route is navigated. Must accept (path, ...captures of matcher)
      * @returns Router
      */
-    add(matcher: string | RegExp, handler: Function): this,
+    add(matcher: string | RegExp, handler: Function) {
+        console.log(`Router.add <= '${matcher}'`);
+        matcher = toRegExp(matcher);
+        this.routes.push({
+            matcher,
+            handler
+        });
+        return this;
+    }
 
     /**
      * Removes a given route.
      * @param matcher - route or RegEx for a series of routes
      * @returns Router
      */
-    remove(matcher: string | RegExp): this,
+    remove(matcher: string | RegExp) {
+        console.log(`Router.remove <= '${matcher}'`);
+        matcher = toRegExp(matcher);
+        this.routes.some((element, index, array) => {
+            if (element.matcher === matcher) {
+                array.splice(index, 1);
+                return true;
+            }
+        });
+        return this;
+    }
 
     /**
      * Removes all routes.
      * @returns Router
      */
-    flush(): this,
+    flush() {
+        console.log('Router.flush');
+        this.routes = [];
+        return this.unlisten();
+    }
 
     /**
      * Sets listener of address bar. Essential to work.
      * @returns Router
      */
-    listen(): this,
+    listen() {
+        console.log('Router.listen');
+        if (!this.listening) {
+            this.listening = true;
+            console.log('Router.listen => HashChange');
+            window.addEventListener('hashchange', this.listener);
+        } else {
+            console.log('Router.listen => already listening');
+        }
+        return this;
+    }
     
     /**
      * Stops listening bar change.
      * @returns Router
      */
-    unlisten(): this,
+    unlisten() {
+        console.log('Router.unlisten');
+        window.removeEventListener('hashchange', this.listener);
+        this.listening = false;
+        return Router;
+    }
 
     /**
      * Searches for the given route (or one in the address bar) and sets its view if found.
@@ -57,90 +130,10 @@ interface Router {
      * @param route
      * @returns Router
      */
-    check(route?: string): this,
-
-    /**
-     * Changes the address bar. If not given, makes force check().
-     * If given, check() is called automatically if Router listens.
-     * @param route
-     * @returns Router
-     */
-    navigate(route?: string): this
-}
-
-const helper = {
-    routes: [] as RouterEntry[],
-    root: '',
-
-    clearSlashes(path: string) {
-        return path.replace(/\/$/, '').replace(/^\//, '');
-    },
-
-    toRegExp(pattern: string | RegExp) {
-        return new RegExp(
-            typeof pattern === 'string'
-                ? `^${pattern}$`
-                : pattern
-        );
-    },
-
-    getFragment() {
-        let match = window.location.href.match(/#(.*)$/);
-        let fragment = match ? match[1] : '';
-        return helper.clearSlashes(fragment);
-    },
-
-    listening: false,
-    current: '',
-    listener() {
-        let fragment = helper.getFragment();
-        if (helper.current !== fragment) {
-            console.log(`HashChange <= '${fragment}'`);
-            helper.current = fragment;
-            Router.check();
-        }
-    }
-};
-
-/** Singleton Router object for Single-Page Apps. */
-const Router = {
-    root(root: string) {
-        helper.root = root;
-        return Router;
-    },
-
-    add(matcher: string | RegExp, handler: Function) {
-        console.log(`Router.add <= '${matcher}'`);
-        matcher = helper.toRegExp(matcher);
-        helper.routes.push({
-            matcher,
-            handler
-        });
-        return Router;
-    },
-
-    remove(matcher: string | RegExp) {
-        console.log(`Router.remove <= '${matcher}'`);
-        matcher = helper.toRegExp(matcher);
-        helper.routes.some((element, index, array) => {
-            if (element.matcher === matcher) {
-                array.splice(index, 1);
-                return true;
-            }
-        });
-        return Router;
-    },
-
-    flush() {
-        console.log('Router.flush');
-        helper.routes = [];
-        return Router.unlisten();
-    },
-
     check(route?: string) {
-        route = route || helper.getFragment();
+        route = route || getFragment();
         console.log(`Router.check <= '${route}'`);
-        let res = helper.routes.some(element => {
+        let res = this.routes.some(element => {
             let match = route.match(element.matcher);
             if (match !== null) {
                 console.log(`Router.check => '${route}' === '${match[0]}'`);
@@ -151,39 +144,26 @@ const Router = {
         });
         if (!res) {
             console.log(`Router.check => none`);
-            Router.navigate(helper.root);
+            this.navigate(this._root);
         }
         return Router;
-    },
+    }
 
-    listen() {
-        console.log('Router.listen');
-        if (!helper.listening) {
-            helper.listening = true;
-            console.log('Router.listen => HashChange');
-            window.addEventListener('hashchange', helper.listener);
-        } else {
-            console.log('Router.listen => already listening');
-        }
-        return Router;
-    },
-
-    unlisten() {
-        console.log('Router.unlisten');
-        window.removeEventListener('hashchange', helper.listener);
-        helper.listening = false;
-        return Router;
-    },
-
+    /**
+     * Changes the address bar. If not given, makes force check().
+     * If given, check() is called automatically if Router listens.
+     * @param route
+     * @returns Router
+     */
     navigate(route?: string) {
         if (route) {
             console.log(`Router.navigate <= '${route}'`);
             window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + route;
         } else {
-            Router.check();
+            this.check();
         }
-        return Router;
+        return this;
     }
-} as Router;
-
-export default Router;
+}
+const router = new Router();
+export default router;
